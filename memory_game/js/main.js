5850312689dd5c2e3d6cb9b74298508dad1d253e
Amadea Kimmins <amadeakimmins@Amadeas-MacBook-Pro-2.local>
2017-11-15 14:02:39 +0000

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,20 +1,23 @@
-
 var cards = ["queen" , "queen" , "king" , "king"];
 var cardsInPlay = [];
+var cardId = cards[0,1,2,3];
 
-var cardOne = cards[0];
-cardsInPlay.push(cardOne);
-console.log("User flipped queen");
+var checkForMatch = function () {
+		if (cardsInPlay[0] === cardsInPlay[1]) {
+			alert("You found a match!");
+		} else {
+			alert("Sorry, try again.");	
+	}
+};
 
-var cardTwo = cards[2];
-cardsInPlay.push(cardTwo);
-console.log("User flipped king");
+var flipCard = function (cardId) {
+	console.log("User flipped " + cards[cardId]);	
+	
+	cardsInPlay.push(cards[cardId]);
 
-if (cardsInPlay.length === 2) {
-} if (cardsInPlay[0] === cardsInPlay[1]) {
-	alert("You found a match!");
-} else {
-	alert("Sorry, try again.");
-}	
+	checkForMatch();
+};
 
 
+flipCard(0);
+flipCard(2);
